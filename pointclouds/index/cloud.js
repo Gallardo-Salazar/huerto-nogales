2330f8b65f97da53f5641786f9b2f03321f45b7a
unknown <pe_gasa@hotmail.com>
2021-08-31 17:35:09 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2908094,
+    "boundingBox": {
+        "lx": 602987.662399292,
+        "ly": 2624123.8722229006,
+        "lz": 1879.2037963867188,
+        "ux": 603286.112411499,
+        "uy": 2624422.3222351076,
+        "uz": 2177.65380859375
+    },
+    "tightBoundingBox": {
+        "lx": 602987.662,
+        "ly": 2624123.872,
+        "lz": 1879.2040000000002,
+        "ux": 603286.112,
+        "uy": 2624391.498,
+        "uz": 1953.473
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 2.584652900695801,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
